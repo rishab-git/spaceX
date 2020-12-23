6046f8ef6a3224feaf15ecc5344f2c30d2cd3fa0
Rishabh Shukla <rishab.akg@gmail.com>
2020-12-23 22:58:26 +0530

    trying to run on Heroku

--- server.ts
+++ server.ts
@@ -49,7 +49,7 @@ function run(): void {
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
+/*// Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
@@ -57,6 +57,13 @@ const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
-}
+}*/
+
+// Start up the Node server
+const PORT = process.env.PORT || 4000;
+const server = app();
+server.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
 
 export * from './src/main.server';
